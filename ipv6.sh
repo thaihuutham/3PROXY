1cf023b927913424ac2bf0dbf5694261bf9add04
thaihuutham <40970590+thaihuutham@users.noreply.github.com>
2023-10-17 09:31:45 +0000

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -46,6 +46,17 @@ then
 	IPV6ADDR=2403:6a40:2000:244::$IPD:0000/64
 	IPV6_DEFAULTGW=2403:6a40:2000:244::1
 	EOF
+elif [ $IPC == 247 ]
+then
+   tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
+	IPV6INIT=yes
+	IPV6_AUTOCONF=no
+	IPV6_DEFROUTE=yes
+	IPV6_FAILURE_FATAL=no
+	IPV6_ADDR_GEN_MODE=stable-privacy
+	IPV6ADDR=2403:6a40:2000:247::$IPD:0000/64
+	IPV6_DEFAULTGW=2403:6a40:2000:247::1
+	EOF
 else
 	tee -a /etc/sysconfig/network-scripts/ifcfg-eth0 <<-EOF
 	IPV6INIT=yes
